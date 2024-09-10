e5b17383ea90368d0319e36069bab8423efb0db2
Rafael Matte <rafael.matte@meliuz.com.br>
2024-09-10 19:43:24 -0300

    chore: Add database connection and reading

---
+++ src/app/types/index.ts
@@ -0,0 +1,9 @@
+export type Trial = {
+  trialCode: string;
+  trialAcronym: string;
+  trialTitle: string;
+  trialUrl: string;
+  trialPhase: string;
+  trialStatus: string;
+  trialCompletionDate: string;
+};
---
+++ src/db/client.ts
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import { parse } from 'fast-csv';
+import type { Trial } from '@/app/types';
+
+const filePath = 'src/db/trialsDatabase.csv';
+const defaultOptions = { headers: true };
+
+let data: Trial[] | null = null;
+
+export const getData = async () => {
+  return data || (await readCsvFile());
+};
+
+const readCsvFile: () => Promise<Trial[]> = () =>
+  new Promise((resolve, reject) => {
+    const content: Trial[] = [];
+
+    fs.createReadStream(filePath)
+      .pipe(parse(defaultOptions))
+      .on('error', (error) => {
+        console.log(error);
+        reject([]);
+      })
+      .on('data', (row) => {
+        content.push(row);
+      })
+      .on('end', () => {
+        data = content;
+        resolve(content);
+      });
+  });
