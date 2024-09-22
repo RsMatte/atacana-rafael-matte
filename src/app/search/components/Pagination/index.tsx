d153f24a5499aaa18983a1f70087457be4144b78
Rafael Matte <rafael.matte@meliuz.com.br>
2024-09-22 14:56:40 -0300

    feat: Improve pagination

--- src/app/search/components/Pagination/index.tsx
+++ src/app/search/components/Pagination/index.tsx
@@ -16,27 +16,42 @@ const Pagination = ({
   status,
 }: PaginationProps) => {
   const { values }: FormikContextType<FormValues> = useFormikContext();
+  const { count, trials } = trialsData;
 
   const isLoading = status === 'loading';
   const isPreviousPageDisabled = isLoading || currentPage === firstPage;
-  const isNextPageDisabled = isLoading || trialsData.trials.length < limit;
+  const isNextPageDisabled = isLoading || count <= currentPage * limit;
+  const totalPages = Math.ceil(count / limit) || 1;
+
+  const getIntervalLabel = () => {
+    if (trials.length === 0) return 'showing 0 results';
+
+    const lowerInterval = (currentPage - 1) * limit + 1;
+    const higherInterval = lowerInterval + trials.length - 1;
+
+    return `showing ${lowerInterval} - ${higherInterval} of ${count} results`;
+  };
 
   return (
-    <div className="filter-pagination">
-      <button
-        className="button-secondary"
-        disabled={isPreviousPageDisabled}
-        onClick={() => clickPreviousPage(values)}
-      >
-        ‹
-      </button>
-      <button
-        className="button-secondary"
-        disabled={isNextPageDisabled}
-        onClick={() => clickNextPage(values)}
-      >
-        ›
-      </button>
+    <div className="pagination">
+      <div className="pagination-buttons">
+        <button
+          disabled={isPreviousPageDisabled}
+          onClick={() => clickPreviousPage(values)}
+        >
+          ‹
+        </button>
+        <span>
+          page {currentPage} of {totalPages}
+        </span>
+        <button
+          disabled={isNextPageDisabled}
+          onClick={() => clickNextPage(values)}
+        >
+          ›
+        </button>
+      </div>
+      <span className="pagination-results">{getIntervalLabel()}</span>
     </div>
   );
 };
